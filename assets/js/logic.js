1e1de923b08b5465e72762fa2307569ea4f81765
Dimi Tsonev <dconev@gmail.com>
2023-12-04 16:43:09 +0000

    getiing DOM elements

---
+++ assets/js/logic.js
@@ -0,0 +1,21 @@
+import questions from "./questions";
+
+//  getting DOM elements
+const time = document.querySelector("#time");
+const startScreen = document.querySelector("#start-screen");
+const startButton = document.querySelector("#start");
+const questions = document.querySelector("#questions");
+const questionTitle = document.querySelector("#question-title");
+const choices = document.querySelector("#choices");
+const endScreen = document.querySelector("#end-screen");
+const finalScore = document.querySelector("#final-score");
+const submitBtn = document.querySelector("#submit");
+const feedback = document.querySelector("#feedback");
+
+function showQuestion(question) {
+  const ulEl = document.createElement('ul')
+  const choices Arr
+}
+
+
+
